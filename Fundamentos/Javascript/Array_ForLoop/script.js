2c43c8f81cdda59e67357706aaad82cce05d7fa2
Kazuo Abduch <kazuo.abduch@gmail.com>
2021-08-26 17:00:07 -0300

    Correção exercicio bonus 2

--- Fundamentos/Javascript/Array_ForLoop/script.js
+++ Fundamentos/Javascript/Array_ForLoop/script.js
@@ -1,6 +1,5 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
 function exercicio1 (){
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
     for (let i = 0; i < numbers.length; i++){
@@ -10,6 +9,7 @@ function exercicio1 (){
 }
 
 function exercicio2 (){
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
     let sum = 0;
     for (let i = 0; i < numbers.length; i++){
@@ -19,6 +19,7 @@ function exercicio2 (){
 }
 
 function exercicio3 (){
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
     // A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
     let sum = 0;
@@ -32,6 +33,7 @@ function exercicio3 (){
 }
 
 function exercicio4 (){
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
     // Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
     let sum = 0;
@@ -50,6 +52,7 @@ function exercicio4 (){
 }
 
 function exercicio5 (){
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
     for (let i = 0; i < numbers.length; i++){
@@ -62,7 +65,7 @@ function exercicio5 (){
 }
 
 function exercicio6 () {
-
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
     let numImpar=0;
@@ -81,7 +84,7 @@ function exercicio6 () {
 }
 
 function exercicio7 (){
-
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
     let menorNum = 20;
@@ -99,6 +102,7 @@ function exercicio7 (){
 }
 
 function exercicio8 () {
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
     let arrayNum = [];
 
@@ -109,6 +113,7 @@ function exercicio8 () {
 }
 
 function exercicio9 (){
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
     let arrayNum = [];
 
@@ -122,40 +127,55 @@ function exercicio9 (){
 }
 
 function exercicioBonus1() {
-    let vetorBonus = [5, 3, 2, 4, 7, 1, 0, 6];
 
-    for (let index = 1; index < vetorBonus.length; index += 1) {
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Ordene o array numbers em ordem crescente e imprima seus valores;
+// Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+    for (let index = 1; index < numbers.length; index += 1) {
         for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-          if (vetorBonus[index] < vetorBonus[secondIndex]) {
-            let position = vetorBonus[index];
-            vetorBonus[index] = vetorBonus[secondIndex];
-            vetorBonus[secondIndex] = position;
+          if (numbers[index] < numbers[secondIndex]) {
+            let position = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = position;
           }
         }
     }
-    console.log(vetorBonus);
-    for (let index = 1; index < vetorBonus.length; index += 1) {
+    console.log(numbers);
+
+    for (let index = 1; index < numbers.length; index += 1) {
         for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-          if (vetorBonus[index] > vetorBonus[secondIndex]) {
-            let position = vetorBonus[index];
-            vetorBonus[index] = vetorBonus[secondIndex];
-            vetorBonus[secondIndex] = position;
+          if (numbers[index] > numbers[secondIndex]) {
+            let position = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = position;
           }
         }
     }
-    console.log(vetorBonus);
+    console.log(numbers);
 }
 
 function exercicioBonus2(){
 
-    let vetorBonus = [5, 3, 2, 4, 7, 1, 0, 6];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Agora crie um novo array a partir do array numbers , sem perdê-lo. 
+// Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. 
+// Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). 
+// Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. 
+// Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo:
+
     let vetorBonusxNove = [];
 
-    for (let i = 0 ; i < vetorBonus.length ; i++){
-        vetorBonusxNove.push(vetorBonus[i]*9);  
-
+    for (let i = 0 ; i < numbers.length ; i++){
+        if(i+1 == numbers.length){
+            vetorBonusxNove.push(numbers[i]*2); 
+        }else {
+            vetorBonusxNove.push(numbers[i]*numbers[i+1]);
+        }
     }
-    console.log(vetorBonus);
+    console.log(numbers);
     console.log(vetorBonusxNove);
 }
 
